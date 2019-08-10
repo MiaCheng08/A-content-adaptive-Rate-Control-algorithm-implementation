7b951104c4a4a8f2aeb289d55dc4a9b105f80f1f
MiaCheng <30514913+MiaCheng08@users.noreply.github.com>
2019-08-10 16:37:47 +0800

    Update CallByCodec_packing.py

--- CallByCodec_packing.py
+++ CallByCodec_packing.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 """
-针对使用阶段的库函数
+
 """
 
 from tensorflow import keras
@@ -20,20 +20,6 @@ def notZero(x):
 
 def parseLookahead(path):
     round = 1
-    cost = []
-    ssdY = []
-    ssdU = []
-    ssdV = []
-    sumY = []
-    sumU = []
-    sumV = []
-    AC = []
-    MVX = []
-    MVY = []
-    MV = []
-    words = []
-    IMB = []
-
     avgScore = {}
     len = 0
     csvFile = open(path, 'r', encoding='UTF-8')
@@ -41,68 +27,7 @@ def parseLookahead(path):
     fieldnames = next(reader)
     reader = csv.DictReader(csvFile, fieldnames=fieldnames, delimiter=',')
     for row in reader:
-        if row['Encode Order'] != "one round LOOKAHEAD is over":
-            #解析look ahead信息
-            avgScore.setdefault(round, {})  # round用来标记当前字典记录的是第几轮look ahead的pre结果
-            len += 1
-            avgScore[round]['end F'] = row['POC']
-            avgScore[round].setdefault('block8Num', row['block8Num'])
-            # avgScore.setdefault('fps', row['fps'])
-            # avgScore.setdefault('block8Num', row['block8Num'])
-            # avgScore.setdefault('block8Num', row['block8Num'])
-            cost.append(float(row['EstCost']))
-            ssdY.append(float(row['wp_ssdY']))
-            ssdU.append(float(row['wp_ssdU']))
-            ssdV.append(float(row['wp_ssdV']))
-            sumY.append(float(row['wp_sumY']))
-            sumU.append(float(row['wp_sumU']))
-            sumV.append(float(row['wp_sumV']))
-            AC.append(float(row['AC']))
-            if row['MVX'] != '-':
-                MVX.append(float(row['MVX']))
-                MVY.append(float(row['MVY']))
-                MV.append(float(row['MV']))
-                words.append(float(row['words']))
-            if row['IMB'] != '-':
-                IMB.append(float(row['IMB']))
-        else:
-            #一轮look ahead解析结束
-            avgScore[round]['Look Len'] = len
-            avgScore[round]['start F'] = int(avgScore[round]['end F']) + 1 - len
-            cost = list(filter(notZero, cost))
-            avgScore[round]['EstCost'] = np.mean(cost)
-            ssdY = list(filter(notZero, ssdY))
-            avgScore[round]['ssdY'] = np.mean(ssdY)
-            ssdU = list(filter(notZero, ssdU))
-            avgScore[round]['ssdU'] = np.mean(ssdU)
-            ssdV = list(filter(notZero, ssdV))
-            avgScore[round]['ssdV'] = np.mean(ssdV)
-            sumY = list(filter(notZero, sumY))
-            avgScore[round]['sumY'] = np.mean(sumY)
-            sumU = list(filter(notZero, sumU))
-            avgScore[round]['sumU'] = np.mean(sumU)
-            sumV = list(filter(notZero, sumV))
-            avgScore[round]['sumV'] = np.mean(sumV)
-            AC = list(filter(notZero, AC))
-            avgScore[round]['AC'] = np.mean(AC)
-            avgScore[round]['MVX'] = 0 if isnan(np.mean(MVX)) else np.mean(MVX)
-            avgScore[round]['MVY'] = 0 if isnan(np.mean(MVY)) else np.mean(MVY)
-            avgScore[round]['MV'] = 0 if isnan(np.mean(MV)) else np.mean(MV)
-            avgScore[round]['words'] = 0 if isnan(np.mean(words)) else np.mean(words)
-            avgScore[round]['IMB'] = 0 if isnan(np.mean(IMB)) else np.mean(IMB)
-            round += 1
-            len = 0
-            cost = []
-            ssdY = []
-            ssdU = []
-            ssdV = []
-            sumY = []
-            sumU = []
-            sumV = []
-            AC = []
-            MVX = []
-            MVY = []
-            IMB = []
+        ****** #confidential
     csvFile.close()
     return avgScore
 
@@ -146,10 +71,7 @@ if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     pred_param = {}
     record_features = {}
-    featureList = ['fps',  'EstCost', 'ssdY', 'ssdU', 'ssdV', 'sumY', 'sumU',
-                   'sumV', 'AC', 'IMB', 'MV', 'words']
-    # featureList = ['weight', 'height','origBitrate','fps',  'EstCost', 'ssdY', 'ssdU', 'ssdV', 'sumY', 'sumU',
-    #                'sumV', 'AC', 'IMB', 'MVX', 'MVY']
+    featureList = ******#confidential
 
     pred_param['modelPath'] = "model.h5"
     pred_param['existedfeaturePath'] = "data.txt"
@@ -157,17 +79,17 @@ if __name__ == '__main__':
     optionWriter = open(optionPath)
     string = optionWriter.read().split("--")
     coding_option = {}
-    # 解析编码命令行参数
+
     for option in string:
         if option:
             key, value = option.split(" ",1)
             coding_option[key] = value
-    featurePath = coding_option['feature']  # 编码器输出look ahead特征文件所在路径
-    feature_Lookahead = parseLookahead(featurePath)#获取look ahead特征, 包括当前特征cover的起始和终点视频帧
+    featurePath = coding_option['feature'] 
+    feature_Lookahead = parseLookahead(featurePath)
 
-    pred_param['videopath'] = coding_option['input']#原始视频所在路径，获取他用来做深入的特征提取
+    pred_param['videopath'] = coding_option['input']
 
-    pred_param['targetR'] = coding_option['targetR']#2.3 Mbps
+    pred_param['targetR'] = coding_option['targetR']
 
     record_features['fps'] = coding_option['fps']
 
@@ -176,7 +98,6 @@ if __name__ == '__main__':
     for item in feature_Lookahead[1]:
         record_features.setdefault(item, float(feature_Lookahead[1][item]))
 
-    record_features['EstCost'] = record_features['EstCost'] / record_features['block8Num']
     pred_param['curr_feature'] = []
     for item in featureList:
         pred_param['curr_feature'].append(float(record_features[item]))
